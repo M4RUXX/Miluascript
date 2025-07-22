12d0146ea0cfbc7dee860ae47a643fe1f2e266a3
M4RUXX <ezzattymarwan@gmail.com>
2025-07-22 23:28:47 +0200

    Update brainrot-teleport.lua

--- brainrot-teleport.lua
+++ brainrot-teleport.lua
@@ -385,7 +385,7 @@ Converted["_FloatingButton"].Size = UDim2.new(0, 50, 0, 100)
 Converted["_FloatingButton"].Visible = false
 Converted["_FloatingButton"].Name = "FloatingButton"
 Converted["_FloatingButton"].Parent = Converted["_M4RU"]
-Convert["_UIPadding5"].PaddingBottom = UDim.new(0, 5)
+Converted["_UIPadding5"].PaddingBottom = UDim.new(0, 5)
 Converted["_UIPadding5"].PaddingLeft = UDim.new(0, 5)
 Converted["_UIPadding5"].PaddingRight = UDim.new(0, 5)
 Converted["_UIPadding5"].PaddingTop = UDim.new(0, 5)
@@ -1464,7 +1464,7 @@ do -- Fake Module: StarterGui.M4RU.FUNCTIONS
 		
 		function FUNCTIONSmodule.finetuneFloatingButton(button: TextButton, dragger)
 			getgenv().fBSFRealButton = button
-            for _, b in ipairs(script.Parent.FloatingButtons:GetChildren()) do
+			for _, b in ipairs(script.Parent.FloatingButtons:GetChildren()) do
 				if b:IsA("TextButton") then
 					local buttonScale = Instance.new("UIScale", b)
 					ts:Create(buttonScale, TweenInfo.new(0.3), {
@@ -2310,7 +2310,7 @@ do -- Fake Module: StarterGui.M4RU.Theme
 			for colorKey, color in api.colors do
 				api:updateColor(colorKey, color)
 			end
-		e=nd
+		end
 		
 		getgenv().ThemeManager = api
 		getgenv().ThemeObjects = themeObjects
@@ -2984,7 +2984,7 @@ local function UTRFSE_fake_script() -- Fake Script: StarterGui.M4RU.Universal
 	function toTokens(str)
 		local tokens = {}
 		for op,name in string.gmatch(str,"([+-])([^+-]+)") do
-			table.insert(tokens,{Operator = op,Name  = name})
+			table.insert(tokens,{Operator = op,Name = name})
 		end
 		return tokens
 	end
@@ -3631,8 +3631,8 @@ local function UTRFSE_fake_script() -- Fake Script: StarterGui.M4RU.Universal
 	end
 	
 	table.insert(module, {
-		Type = "Text"
-        Args = {"Fling"}
+		Type = "Text",
+		Args = {"Fling"}
 	})
 	
 	local playerToFling
@@ -4066,8 +4066,8 @@ local function UTRFSE_fake_script() -- Fake Script: StarterGui.M4RU.Universal
 	
 	table.insert(module, {
 		Type = "Text",
-		Args = {"Other"}	
-    })
+		Args = {"Other"}
+	})
 	
 	--table.insert(module, {
 	--	Type = "Button",
@@ -5313,7 +5313,7 @@ local function QUXUEEW_fake_script() -- Fake Script: StarterGui.M4RU.Murder Myst
 		return nearestPlayer
 	end
 
-    function miniFling(playerToFling)
+	function miniFling(playerToFling)
 		local a=game.Players.LocalPlayer;local b=a:GetMouse()local c={playerToFling}local d=game:GetService("Players")local e=d.LocalPlayer;local f=false;local g=function(h)local i=e.Character;local j=i and i:FindFirstChildOfClass("Humanoid")local k=j and j.RootPart;local l=h.Character;local m;local n;local o;local p;local q;if l:FindFirstChildOfClass("Humanoid")then m=l:FindFirstChildOfClass("Humanoid")end;if m and m.RootPart then n=m.RootPart end;if l:FindFirstChild("Head")then o=l.Head end;if l:FindFirstChildOfClass("Accessory")then p=l:FindFirstChildOfClass("Accessory")end;if p and p:FindFirstChild("Handle")then q=p.Handle end;if i and j and k then if k.Velocity.Magnitude<50 then getgenv().OldPos=k.CFrame end;if m and m.Sit and not f then end;if o then if o.Velocity.Magnitude>500 then fu.dialog("Player flung","Player is already flung. Fling again?",{"Fling again","No"})if fu.waitfordialog()=="No"then return fu.closedialog()end;fu.closedialog()end elseif not o and q then if q.Velocity.Magnitude>500 then fu.dialog("Player flung","Player is already flung. Fling again?",{"Fling again","No"})if fu.waitfordialog()=="No"then return fu.closedialog()end;fu.closedialog()end end;if o then workspace.CurrentCamera.CameraSubject=o elseif not o and q then workspace.CurrentCamera.CameraSubject=q elseif m and n then workspace.CurrentCamera.CameraSubject=m end;if not l:FindFirstChildWhichIsA("BasePart")then return end;local r=function(s,t,u)k.CFrame=CFrame.new(s.Position)*t*u;i:SetPrimaryPartCFrame(CFrame.new(s.Position)*t*u)k.Velocity=Vector3.new(9e7,9e7*10,9e7)k.RotVelocity=Vector3.new(9e8,9e8,9e8)end;local v=function(s)local w=2;local x=tick()local y=0;repeat if k and m then if s.Velocity.Magnitude<50 then y=y+100;r(s,CFrame.new(0,1.5,0)+m.MoveDirection*s.Velocity.Magnitude/1.25,CFrame.Angles(math.rad(y),0,0))task.wait()r(s,CFrame.new(0,-1.5,0)+m.MoveDirection*s.Velocity.Magnitude/1.25,CFrame.Angles(math.rad(y),0,0))task.wait()r(s,CFrame.new(2.25,1.5,-2.25)+m.MoveDirection*s.Velocity.Magnitude/1.25,CFrame.Angles(math.rad(y),0,0))task.wait()r(s,CFrame.new(-2.25,-1.5,2.25)+m.MoveDirection*s.Velocity.Magnitude/1.25,CFrame.Angles(math.rad(y),0,0))task.wait()r(s,CFrame.new(0,1.5,0)+m.MoveDirection,CFrame.Angles(math.rad(y),0,0))task.wait()r(s,CFrame.new(0,-1.5,0)+m.MoveDirection,CFrame.Angles(math.rad(y),0,0))task.wait()else r(s,CFrame.new(0,1.5,m.WalkSpeed),CFrame.Angles(math.rad(90),0,0))task.wait()r(s,CFrame.new(0,-1.5,-m.WalkSpeed),CFrame.Angles(0,0,0))task.wait()r(s,CFrame.new(0,1.5,m.WalkSpeed),CFrame.Angles(math.rad(90),0,0))task.wait()r(s,CFrame.new(0,1.5,n.Velocity.Magnitude/1.25),CFrame.Angles(math.rad(90),0,0))task.wait()r(s,CFrame.new(0,-1.5,-n.Velocity.Magnitude/1.25),CFrame.Angles(0,0,0))task.wait()r(s,CFrame.new(0,1.5,n.Velocity.Magnitude/1.25),CFrame.Angles(math.rad(90),0,0))task.wait()r(s,CFrame.new(0,-1.5,0),CFrame.Angles(math.rad(90),0,0))task.wait()r(s,CFrame.new(0,-1.5,0),CFrame.Angles(0,0,0))task.wait()r(s,CFrame.new(0,-1.5,0),CFrame.Angles(math.rad(-90),0,0))task.wait()r(s,CFrame.new(0,-1.5,0),CFrame.Angles(0,0,0))task.wait()end else break end until s.Velocity.Magnitude>500 or s.Parent~=h.Character or h.Parent~=d or h.Character~=l or m.Sit or j.Health<=0 or tick()>x+w end;workspace.FallenPartsDestroyHeight=0/0;local z=Instance.new("BodyVelocity")z.Name="EpixVel"z.Parent=k;z.Velocity=Vector3.new(9e8,9e8,9e8)z.MaxForce=Vector3.new(1/0,1/0,1/0)j:SetStateEnabled(Enum.HumanoidStateType.Seated,false)if n and o then if(n.CFrame.p-o.CFrame.p).Magnitude>5 then v(o)else v(n)end elseif n and not o then v(n)elseif not n and o then v(o)elseif not n and not o and p and q then v(q)else fu.notification("Can't find a proper part of target player to fling.")end;z:Destroy()j:SetStateEnabled(Enum.HumanoidStateType.Seated,true)workspace.CurrentCamera.CameraSubject=j;repeat k.CFrame=getgenv().OldPos*CFrame.new(0,.5,0)i:SetPrimaryPartCFrame(getgenv().OldPos*CFrame.new(0,.5,0))j:ChangeState("GettingUp")table.foreach(i:GetChildren(),function(A,B)if B:IsA("BasePart")then B.Velocity,B.RotVelocity=Vector3.new(),Vector3.new()end end)task.wait()until(k.Position-getgenv().OldPos.p).Magnitude<25;workspace.FallenPartsDestroyHeight=getgenv().FPDH else fu.notification("No valid character of said target player. May have died.")end end;g(c[1])
 	end
 
@@ -6328,5 +6328,3 @@ coroutine.wrap(QUXUEEW_fake_script)()
 coroutine.wrap(PIXKC_fake_script)()
 coroutine.wrap(JIUI_fake_script)()
 coroutine.wrap(ZISNWBG_fake_script)()
-
-		
